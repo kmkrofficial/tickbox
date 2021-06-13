716f4a044f0bb9e6c9d66a8119e09adf75fafc3b
Keerthi Raajan <kmkrworks@gmail.com>
2021-06-13 19:29:05 +0530

    v2.2 Development Version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
 from pipeline import getCosineSimilarityFromOnline, getCosineSimilarityFromDrive
@@ -8,17 +8,17 @@ api = Api(app)
 
 
 class TickBoxOnline(Resource):
-    def get(self, question, file_name):
-        return getCosineSimilarityFromOnline(question, file_name)
+    def post(self, question, file_name):
+        print("Success")
+        return jsonify(getCosineSimilarityFromOnline(question, file_name))
 
 
 class TickBoxDrive(Resource):
-    def get(self, question, file_name):
-        return getCosineSimilarityFromDrive(question, file_name)
+    def post(self, question, file_name):
+        return jsonify(getCosineSimilarityFromDrive(question, file_name))
 
 
 api.add_resource(TickBoxOnline, "/to/<string:question>/<string:file_name>")
-
 api.add_resource(TickBoxDrive, "/td/<string:question>/<string:file_name>")
 
 if __name__ == '__main__':
